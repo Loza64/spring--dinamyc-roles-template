9725470f5c26c2b74a0b19c2c1e3a4c36a9a0203
loza.dev <robertoloxa88@gmail.com>
2026-01-10 11:39:12 -0600

    feat update pagination model

--- src/main/java/com/server/app/dto/response/Pagination.java
+++ src/main/java/com/server/app/dto/response/Pagination.java
@@ -3,9 +3,9 @@ package com.server.app.dto.response;
 import java.util.List;
 
 public record Pagination<T>(
-                List<T> data,
-                int page,
-                int size,
-                int pages,
-                long total) {
+        List<T> data,
+        int page,
+        int pageSize,
+        int pageCount,
+        long total) {
 }
